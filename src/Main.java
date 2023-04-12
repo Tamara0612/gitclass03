ce4fa78b1189ad6f23e483e36132477d96a7a39a
Tamara0612 <tamarabg0612@gmail.com>
2023-04-11 17:52:41 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("i am a project");
 
-        
+        System.out.println("i am a contiuation to project");
 
 }
 }
